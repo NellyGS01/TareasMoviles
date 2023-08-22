11c48f4eefbeb41fb30ebc39dc409ab56a4f307e
NellyGS01 <100099928+NellyGS01@users.noreply.github.com>
2023-08-22 15:24:43 -0600

    Finalizacion de Tarea1

--- tarea_1/lib/main.dart
+++ tarea_1/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-/// Flutter code sample for [Card].
+/// Flutter code sample for [IconButton] with toggle feature.
 
 void main() {
   runApp(const McFlutter());
@@ -12,47 +12,84 @@ class McFlutter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-          colorSchemeSeed: const Color(0xff6750a4), useMaterial3: true),
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: false,
-          backgroundColor: Color.fromARGB(255, 0, 106, 255),
-          title:
-              const Text('Mc Flutter', style: TextStyle(color: Colors.white)),
-        ),
-        body: const Column(
-          children: <Widget>[
-            OutlinedCardExample(),
-          ],
-        ),
-      ),
-    );
+        theme: ThemeData(
+            colorSchemeSeed: const Color(0xff6750a4), useMaterial3: true),
+        title: 'Icon Button Types',
+        home: Scaffold(
+          appBar: AppBar(
+            centerTitle: false,
+            backgroundColor: const Color.fromARGB(255, 0, 106, 255),
+            title:
+                const Text('Mc Flutter', style: TextStyle(color: Colors.white)),
+          ),
+          body: const Column(
+            children: <Widget>[
+              DemoIconToggleButtons(),
+            ],
+          ),
+        ));
   }
 }
 
-class OutlinedCardExample extends StatelessWidget {
-  const OutlinedCardExample({super.key});
+class DemoIconToggleButtons extends StatefulWidget {
+  const DemoIconToggleButtons({super.key});
 
   @override
+  State<DemoIconToggleButtons> createState() => _DemoIconToggleButtonsState();
+}
+
+class _DemoIconToggleButtonsState extends State<DemoIconToggleButtons> {
+  bool standardSelected = false;
+  bool standardSelected2 = false;
+  Color _iconColor = Colors.black;
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Card(
-        elevation: 0,
-        shape: RoundedRectangleBorder(
-          side: BorderSide(
-            color: Theme.of(context).colorScheme.outline,
-          ),
-        ),
-        child: const SizedBox(
-          width: double.infinity,
-          height: 150,
-          child: Center(
-              child: Column(
-            children: [
-              Expanded(
-                child: Row(
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Card(
+            elevation: 0,
+            shape: RoundedRectangleBorder(
+              side: BorderSide(
+                color: Theme.of(context).colorScheme.outline,
+              ),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const Row(
+                  children: [
+                    SizedBox(
+                      width: 100,
+                    ),
+                    Icon(
+                      Icons.account_circle,
+                      color: Colors.black,
+                      size: 50.0,
+                    ),
+                    SizedBox(
+                      child: Column(
+                        children: [
+                          Text(
+                            'Flutter McFlutter',
+                            textAlign: TextAlign.start,
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          Text(
+                            'Experienced App Developer',
+                            //textAlign: TextAlign.left,
+                            style: TextStyle(fontSize: 15),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                const Row(
                   children: <Widget>[
+                    SizedBox(
+                      width: 10,
+                    ),
                     Expanded(
                       child: Text('123 Main Street',
                           textAlign: TextAlign.left,
@@ -62,72 +99,78 @@ class OutlinedCardExample extends StatelessWidget {
                       child: Text('(415) 555-0198',
                           textAlign: TextAlign.right,
                           style: TextStyle(fontSize: 15)),
-                    )
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
                   ],
                 ),
-              ),
-              Expanded(
-                child: Row(
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
-                    Expanded(
-                      child: Text('123 Main Street',
-                          textAlign: TextAlign.left,
-                          style: TextStyle(fontSize: 15)),
+                    const Column(
+                      children: [
+                        SizedBox(
+                          width: 3,
+                        )
+                      ],
                     ),
-                    Expanded(
-                      child: Text('(415) 555-0198',
-                          textAlign: TextAlign.right,
-                          style: TextStyle(fontSize: 15)),
-                    )
+                    Column(
+                      children: [
+                        IconButton(
+                          isSelected: standardSelected,
+                          icon: Icon(Icons.accessibility, color: _iconColor),
+                          onPressed: () {
+                            setState(() {
+                              standardSelected = !standardSelected;
+                              if (standardSelected == false) {
+                                _iconColor = Colors.cyan;
+                              } else {
+                                _iconColor = Colors.black;
+                              }
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        IconButton(
+                          isSelected: standardSelected2,
+                          icon: Icon(Icons.timer, color: Colors.black),
+                          onPressed: () {
+                            final snackBar = SnackBar(
+                              content: const Text('Yay! A SnackBar!'),
+                              action: SnackBarAction(
+                                label: 'Undo',
+                                onPressed: () {},
+                              ),
+                            );
+
+                            ScaffoldMessenger.of(context)
+                                .showSnackBar(snackBar);
+                          },
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [Icon(Icons.smartphone, color: Colors.black)],
+                    ),
+                    Column(
+                      children: [Icon(Icons.smartphone, color: Colors.black)],
+                    ),
+                    Column(
+                      children: [
+                        SizedBox(
+                          width: 1,
+                        )
+                      ],
+                    ),
                   ],
                 ),
-              ),
-              Expanded(
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: Icon(
-                        Icons.accessibility,
-                        color: Colors.black,
-                        size: 24.0,
-                        semanticLabel:
-                            'Text to announce in accessibility modes',
-                      ),
-                    ),
-                    Expanded(
-                      child: Icon(
-                        Icons.timer,
-                        color: Colors.black,
-                        size: 24.0,
-                        semanticLabel:
-                            'Text to announce in accessibility modes',
-                      ),
-                    ),
-                    Expanded(
-                      child: Icon(
-                        Icons.smartphone,
-                        color: Colors.black,
-                        size: 24.0,
-                        semanticLabel:
-                            'Text to announce in accessibility modes',
-                      ),
-                    ),
-                    Expanded(
-                      child: Icon(
-                        Icons.smartphone,
-                        color: Colors.black,
-                        size: 24.0,
-                        semanticLabel:
-                            'Text to announce in accessibility modes',
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          )),
-        ),
-      ),
+              ],
+            ))
+      ],
     );
   }
 }
